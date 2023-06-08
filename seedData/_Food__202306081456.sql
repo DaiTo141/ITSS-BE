f10c91c3a46c95395489e9c91e024035b4a3db81
loc <nguyengialoc7@gmail.com>
2023-06-08 15:18:50 +0700

    seed data

---
+++ seedData/_Food__202306081456.sql
@@ -0,0 +1,13 @@
+INSERT INTO public."Food" (description,image,restaurant_id,price,"name",rating_average) VALUES
+	 ('色と味が融合したカニかまの丼。ライスペーパーの赤茶色が黄金色のだし汁に溶け込み、自然な甘さ。 香ばしいカニ肉、脂の乗ったカニレンガ、脂の塊、カリカリに揚げた玉ねぎがハイフォンの名物料理になります。','https://i-giadinh.vnecdn.net/2023/02/25/Buoc-12-thanh-pham-12-3879-1677318715.jpg',1,45000,'バインダークア',0.0),
+	 ('ブンリュークアは、ベトナムで古くから伝わる民間伝統料理の 1 つです。 多彩な素材を使用し、味わいも香りも豊かで、一度食べたら魅了される麺料理です。','https://daotaobeptruong.vn/wp-content/uploads/2020/06/bun-rieu-cua.jpg',2,55000,'ブンリュークア',0.0),
+	 ('フォートロンはその美味しさで多くの人に愛されている料理です。','https://cdn.tgdd.vn/2021/10/CookDish/huong-dan-2-cach-lam-pho-tron-chua-ngot-cuc-ngon-cuc-don-gian-avt-1200x676.jpg',3,50000,'フォートロン',0.0),
+	 ('エビ数匹とカニ入り春雨炒めは、春雨炒めの中でも最高の一品と言えます。 しかも栄養価も高くて食べやすい一品です。','https://www.cet.edu.vn/wp-content/uploads/2018/04/mi-xao-thap-cam.jpg',4,30000,'ミエンサオ',0.0),
+	 ('ブロークンライスはサイゴンの代表的な料理として有名ですが、常に首都のダイナーの心を掴んでいます。','https://product.hstatic.net/200000523823/product/com_tam_suon_bi_cha_b187787e63d9478f8bfc671d36b36a9b_1024x1024.jpg',5,65000,'コムタム',0.0),
+	 ('うなぎは丸ごと焦げ茶色で、パリパリ、透き通った出汁、甘みがあり、柔らかくて食べごたえのある春雨です。','https://i-giadinh.vnecdn.net/2022/11/10/Buoc-6-thanh-pham-6-4302-1668073404.jpg',6,30000,'ミエンウオン',0.0),
+	 ('材料も手に入り、調理法も簡単で、私たちにとって身近なワンタン麺。','https://cdn.tgdd.vn/Files/2021/05/27/1355347/cach-nau-mi-van-than-thom-ngon-chuan-vi-ha-thanh-202201171332512836.jpg',7,30000,'ミーヴァンタン',0.0),
+	 ('独特の風味と加工により、成功したリブ粥は、それを楽しむ人々の心を掴みました。','https://kanawa.vn/wp-content/uploads/2022/08/chao-suon-mien-nam-co-duoc-ua-chuong.jpg',8,60000,'チャオスオン',0.0),
+	 ('ハノイのブレッドパンは、多くの美食家に人気の料理です。','https://statics.vinpearl.com/banh-mi-chao-ha-noi-so-1_1680981247.jpg',9,100000,'バインミーチャオ',0.0),
+	 ('もち米の種は柔らかく、金色で、伸縮性があり、滑らかなインゲンと混ぜられ、さらにカリカリで脂っこいフライドオニオンがとても魅力的です。','https://i-giadinh.vnecdn.net/2021/10/08/nh1-1633673448-8968-1633673738.jpg',10,70000,'ソイ',0.0);
+INSERT INTO public."Food" (description,image,restaurant_id,price,"name",rating_average) VALUES
+	 ('もち米の種は柔らかく、金色で、伸縮性があり、滑らかなインゲンと混ぜられ、さらにカリカリで脂っこいフライドオニオンがとても魅力的です。','https://i-giadinh.vnecdn.net/2021/10/08/nh1-1633673448-8968-1633673738.jpg',1,30000,'ソイ',0.0);
