b2c93681e3eaa3cda2476aa8e8eb1d145fd76cba
loc <nguyengialoc7@gmail.com>
2023-06-21 11:58:36 +0700

    add data

--- seedData/_User__202306081503.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO public."User" ("name",email,"password",image,status) VALUES
-	 ('Nguyễn Gia Lộc','nguyengialoc@gmail.com','123456','https://www.computerhope.com/jargon/g/guest-user.png',1),
-	 ('Lê Danh Đại','ledanhdai@gmail.com','123456','https://www.computerhope.com/jargon/g/guest-user.png',1),
-	 ('Nguyễn Ngọc Anh','nguyenngocanh@gmail.com','123456','https://www.computerhope.com/jargon/g/guest-user.png',1),
-	 ('Lê Đức Phong','leducphong@gmail.com','123456','https://www.computerhope.com/jargon/g/guest-user.png',1),
-	 ('Hà Công Tuấn','hacongtuan@gmail.com','123456','https://www.computerhope.com/jargon/g/guest-user.png',1),
-	 ('Vũ Đức Dũng','vuducdung@gmail.com','123456','https://www.computerhope.com/jargon/g/guest-user.png',1),
-	 ('Phạm Đức Tuấn','phamductuan@gmail.com','123456','https://www.computerhope.com/jargon/g/guest-user.png',1);
